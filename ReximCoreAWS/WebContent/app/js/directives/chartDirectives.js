d8d0b4c106a4d240cdeafc2a7cd897ccf76697f1
perforam <office@Hirans-MacBook-Pro.local>
2013-11-01 15:39:44 +0530

    Merge branch 'master' of https://github.com/reximcore/AtomSplitter
    Conflicts:
    ReximCoreAWS/WebContent/app/js/directives/chartDirectives.js
    ReximCoreAWS/WebContent/app/partials/dashboard.html

--- ReximCoreAWS/WebContent/app/js/directives/chartDirectives.js
+++ ReximCoreAWS/WebContent/app/js/directives/chartDirectives.js
@@ -1,42 +1,81 @@
 'use strict';
 
 /* Directives */
-angular.module('myApp.chartDirectives', []).directive('chart', function(AnalyticsData){
-    return{
-        restrict: 'E',
-        link: function(scope, elem, attrs){            
-            var chart = null;
-            var data = scope[attrs.ngModel];            
-            
-            // If the data changes somehow, update it in the chart
-            scope.$watch('chartData', function(chartData){
-            	console.log(AnalyticsData.getPageCountConfigs());
-            	var ticks =  AnalyticsData.getPageCountConfigs();            	
-            	 var options = {
-		            		series: {
-		            	        bars: {
-		            	            show: true,
-		            	            align: "center",
-		            	            barWidth: 0.7
-		            	        }
-		            	    },
-		            	    xaxis: {
-		            	    	axisLabel: "Pages",
-		            	        axisLabelUseCanvas: false,
-		            	        axisLabelFontSizePixels: 9,
-		            	        axisLabelFontFamily: 'Verdana, Arial',
-		            	        axisLabelPadding: 3,
-		            	        ticks: ticks			            	        
-		            	    },	
-		            	    grid: {
-		                        hoverable: true,
-		                        borderWidth: 0,
-		                        backgroundColor: { colors: ["#ffffff", "#EDF5FF"] }
-		                       }
-		            };
-		              
-            	 chart = $.plot(elem, chartData , options);                 
+
+var chartDirective = angular.module('myApp.chartDirectives', []);
+
+chartDirective.directive('chart', function(AnalyticsData){
+	    return{
+	        restrict: 'E',
+	        link: function(scope, elem, attrs){            
+	            var chart = null;
+	            var data = scope[attrs.ngModel];            
+	            
+	            // If the data changes somehow, update it in the chart
+	            scope.$watch('chartData', function(chartData){
+	            	console.log(AnalyticsData.getPageCountConfigs());
+	            	var ticks =  AnalyticsData.getPageCountConfigs();            	
+	            	 var options = {
+			            		series: {
+			            	        bars: {
+			            	            show: true,
+			            	            align: "center",
+			            	            barWidth: 0.5
+			            	        }
+			            	    },
+			            	    xaxis: {
+			            	    	axisLabel: "Page Count",
+			            	        axisLabelUseCanvas: false,
+			            	        axisLabelFontSizePixels: 10,
+			            	        axisLabelFontFamily: 'Verdana, Arial',
+			            	        axisLabelPadding: 10,
+			            	        ticks: ticks				            	        
+			            	    },
+			            	    grid: {
+			                        hoverable: true,
+			                        borderWidth: 0,
+			                        backgroundColor: { colors: ["#ffffff", "#EDF5FF"] }
+			                       }
+			            };			              
+	            	 chart = $.plot(elem, chartData , options);                 
+	            });
+	        }
+	    };
+	});
+
+chartDirective.directive('piechart', function() {
+    return {
+        restrict: 'E',        
+        link: function(scope, elem, attrs) {
+        	var chart = null;
+            scope.$watch('pieChartUserType', function(v){ 
+            	var options = {
+            			series: {
+		        	        pie: {
+		        	            show: true,
+		        	            label: {
+		        	                show: true,		        	                
+		        	                labelFormatter: function (label, series) {
+		        	                    return '<div style="border:1px solid grey;font-size:8pt;text-align:center;padding:5px;color:white;">' +
+		        	                    label + ' : ' +
+		        	                    Math.round(series.percent) +
+		        	                    '%</div>';
+		        	                },
+		        	        }
+		        	    },
+		        	    grid: {
+		        	        hoverable: true
+		        	    }
+            	}
+            };
+                if(v) 
+                {
+                    chart = $.plot(elem, v, options);                    
+                }
             });
         }
     };
 });
+
+
+	
